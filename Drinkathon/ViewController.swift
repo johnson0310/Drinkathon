da0e41eb33f7535a334cf31e0c8700af730c69a8
Johnson Zhang <zhang_xihan@hotmail.com>
2019-04-18 16:32:56 -0500

    Added Weight input

--- Drinkathon/ViewController.swift
+++ Drinkathon/ViewController.swift
@@ -16,20 +16,32 @@ class ViewController: UIViewController {
     var genderPicker = UIPickerView()
     var genderPickerData : [String] = ["Male","Female"]
     
-    @IBOutlet weak var check: UILabel!
+    
+    //Weight
+    @IBOutlet weak var weightInputTextField: UITextField!
+    var weightPicker = UIPickerView()
+    var weightPickerData : [Int] = [100,110,120,130,140,150,160,170,180,190,200,210,220,230,240]
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         
-        
+        /* Initialilze PickerView */
+        //GenderPicker
+        genderPicker.selectRow(0, inComponent: 0, animated: true)
         genderPicker.delegate = self
         genderPicker.dataSource = self
         
-        genderInputTextField.inputView = genderPicker
+        //WeightPicker
+        weightPicker.selectRow(2, inComponent: 0, animated: true)
+        weightPicker.delegate = self
+        weightPicker.dataSource = self
+
         
-        /* Create Cancel and Done button on UIViewPicker*/
+        /* Create Cancel and Done button on UIPickerView*/
         let toolBar = UIToolbar()
         toolBar.barStyle = UIBarStyle.default
         toolBar.isTranslucent = true
@@ -43,15 +55,23 @@ class ViewController: UIViewController {
         toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
         toolBar.isUserInteractionEnabled = true
         
+        
+        
+        
+        /* Connect PickerView and TextFields */
         genderInputTextField.inputView = genderPicker
         genderInputTextField.inputAccessoryView = toolBar
+        
+        weightInputTextField.inputView = weightPicker
+        weightInputTextField.inputAccessoryView = toolBar
        
         
     }
     
+    //Close the UIPickerView after making a selection
     @objc func donePicker() {
+        weightInputTextField.resignFirstResponder()
         genderInputTextField.resignFirstResponder()
-        
     }
   
 }
@@ -60,20 +80,41 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return genderPickerData.count
+        if pickerView == genderPicker{
+            return genderPickerData.count
+        } else if pickerView == weightPicker{
+            return weightPickerData.count
+        }
+        return 1
     }
     
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        if pickerView == genderPicker{
+            return 1
+        } else if pickerView == weightPicker{
+            return 1
+        }
+        
         return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        genderInputTextField.text = genderPickerData[row]
+        if pickerView == genderPicker{
+            genderInputTextField.text = genderPickerData[row]
+        } else if pickerView == weightPicker{
+            weightInputTextField.text = String(weightPickerData[row])
+        }
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return genderPickerData[row]
+        if pickerView == genderPicker{
+            return genderPickerData[row]
+        } else if pickerView == weightPicker{
+            return String(weightPickerData[row])
+        }
+        
+        return "2"
     }
     
     
