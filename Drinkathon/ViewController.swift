435d251dcac8e6dbfd8c373307b2b133cfafed05
Johnson Zhang <zhang_xihan@hotmail.com>
2019-04-17 23:42:19 -0500

    Initial Commit

---
+++ Drinkathon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Drinkathon
+//
+//  Created by Johnson on 4/17/19.
+//  Copyright © 2019 JohnsonZhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
